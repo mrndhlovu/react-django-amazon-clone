1348717b91b52468e51f19178d8885902bc2784e
Mduduzi Ndhlovu <40129820+mrndhlovu@users.noreply.github.com>
2020-10-24 23:16:13 +0100

    use custom webpack config

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,7 @@ import ReactDOM from "react-dom";
 
 import App from "./js/App";
 import "./assets/theme/sass/styles.scss";
-import reportWebVitals from "./reportWebVitals";
 
 const ROOT_ELEMENT = document.getElementById("root");
 
 ReactDOM.render(<App />, ROOT_ELEMENT);
-
-reportWebVitals();
---
+++ frontend/webpack.prod.js
@@ -0,0 +1,44 @@
+const { merge } = require("webpack-merge");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const shared = require("./webpack.common");
+
+module.exports = merge(shared, {
+  mode: "production",
+  optimization: {
+    minimizer: [new OptimizeCssAssetsPlugin(), new TerserPlugin()],
+  },
+  performance: {
+    hints: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s(a|c)ss|css$/,
+        exclude: /\.module.(s(a|c)ss)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: false,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  output: {
+    filename: "bundle_main.js",
+    hotUpdateChunkFilename: ".hot/[id].hot-update.js",
+    hotUpdateMainFilename: ".hot/.hot-update.json",
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+  ],
+});
